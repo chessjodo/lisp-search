259920e75b615379f0be61a0819c32526c8a46df
Jonas Dornieden <jonas.dornieden@student.auc.nl>
2024-05-30 21:58:21 +0200

    find successors function

--- fdg.lisp
+++ fdg.lisp
@@ -7,7 +7,10 @@
   grain)
 
 (defun make-start ()
-  (make-state :woman 0 :fox 0 :duck 0 :grain 1))
+  (make-state :woman 0 :fox 0 :duck 0 :grain 0))
+
+(defun not-safe-state ()
+  (make-state :woman 0 :fox 0 :duck 1 :grain 1))
 
 (defun is-goal-state ()
   #'(lambda (state)
@@ -21,3 +24,32 @@
                 (= (state-fox state) (state-duck state)))
            (and (not (= (state-woman state) (state-duck state)))
                 (= (state-duck state) (state-grain state))))))
+
+(defun move (thing state)
+  (let ((new-side (if (= (state-woman state) 0) 1 0)))
+    (case thing
+      (:woman (make-state :woman new-side
+                          :fox (state-fox state)
+                          :duck (state-duck state)
+                          :grain (state-grain state)))
+      (:fox (if (= (state-woman state) (state-fox state))
+                (make-state :woman new-side
+                            :fox new-side
+                            :duck (state-duck state)
+                            :grain (state-grain state)) (not-safe-state)))
+      (:duck (if (= (state-woman state) (state-duck state))
+                 (make-state :woman new-side
+                             :fox (state-fox state)
+                             :duck new-side
+                             :grain (state-grain state)) (not-safe-state)))
+      (:grain (if (= (state-woman state) (state-grain state))
+                  (make-state :woman new-side
+                              :fox (state-fox state)
+                              :duck (state-duck state)
+                              :grain new-side) (not-safe-state))))))
+(defun find-successors (state)
+  (remove-if-not #'is-safe-state
+                 (list (move :woman state)
+                       (move :fox state)
+                       (move :duck state)
+                       (move :grain state))))
