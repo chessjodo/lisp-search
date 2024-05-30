c72e5522470c40991268178362eed063a8d478f6
Jonas Dornieden <jonas.dornieden@student.auc.nl>
2024-05-31 01:59:07 +0200

    changed packages

--- package.lisp
+++ package.lisp
@@ -5,7 +5,8 @@
 
 (defpackage :search-assignment
   (:use :cl :iterate :alexandria)
-  (:export :make-state :state :get-left :get-right :state-left :state-right
+  (:export :make-state :state :state-5l :state-3l :move :find-successors
+           :prepend
            :state-p :get-left :get-right :fail :tree-search
            :depth-limited-tree-search :depth-limited-depth-first-search
            :depth-first-search :breadth-first-search :best-first-search
