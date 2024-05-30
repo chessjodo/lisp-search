7834d66b8b3bd10c41e8239e2a019dbb4c85a935
Jonas Dornieden <jonas.dornieden@student.auc.nl>
2024-05-30 21:23:46 +0200

    Goal state function

--- fdg.lisp
+++ fdg.lisp
@@ -8,3 +8,10 @@
 
 (defun make-start ()
   (make-state :woman 0 :fox 0 :duck 0 :grain 1))
+
+(defun is_goal_state ()
+  #'(lambda (state)
+      (and (= (state-woman state) 1)
+           (= (state-duck state) 1)
+           (= (state-fox state) 1)
+           (= (state-grain state) 1))))
