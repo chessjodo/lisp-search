bc719f0f08cae84b96be86109c75ea0ba5abcd18
Jonas Dornieden <jonas.dornieden@student.auc.nl>
2024-05-30 21:28:15 +0200

    function to check for when eliminating unsafe states

--- fdg.lisp
+++ fdg.lisp
@@ -9,9 +9,15 @@
 (defun make-start ()
   (make-state :woman 0 :fox 0 :duck 0 :grain 1))
 
-(defun is_goal_state ()
+(defun is-goal-state ()
   #'(lambda (state)
       (and (= (state-woman state) 1)
            (= (state-duck state) 1)
            (= (state-fox state) 1)
            (= (state-grain state) 1))))
+
+(defun is-safe-state (state)
+  (not (or (and (not (= (state-woman state) (state-fox state)))
+                (= (state-fox state) (state-duck state)))
+           (and (not (= (state-woman state) (state-duck state)))
+                (= (state-duck state) (state-grain state))))))
